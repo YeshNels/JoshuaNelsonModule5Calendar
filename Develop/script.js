f6a6d35876dfb3398f47356e02e10f4254bac182
YeshNels <paleojosh@sbcglobal.net>
2022-11-16 16:24:57 -0800

    Adds all of the timeblocks in html. Adds sweeping script statements for all in the past or future.

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,18 @@
 var currentDay = $("#currentDay")
 
 $("#currentDay").text(moment().format("MMM, Do, YYYY"))
+
+console.log(moment().hours());
+
+var currentHour = moment().hours();
+
+if (currentHour<9) {
+    $(".timeblock").addClass("future");
+    $(".timeblock").removeClass("past");
+    $(".timeblock").removeClass("present");
+}
+if (currentHour>17) {
+    $(".timeblock").addClass("past");
+    $(".timeblock").removeClass("future");
+    $(".timeblock").removeClass("present");
+}
